b603cfd25ad9a7b569e97caed38394deedd46263
alexa <alexa@ALEXANDRA>
2023-03-14 23:48:08 -0600

    Agregar archivos de proyecto.

---
+++ P4-Examen-120350048/Models/Localizacion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P4_Examen_120350048.Models
+{
+    internal class Localizacion
+     {
+
+    public Double latitud { get; set; }
+
+    public Double longitud { get; set; }
+
+    public String descripcion_larga { get; set; }
+
+    public String descripcion_corta { get; set; }
+
+     }
+}
---
+++ P4-Examen-120350048/Views/PageLocalizacion.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+
+
+namespace P4_Examen_120350048.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageLocalizacion : ContentPage
+    {
+        public PageLocalizacion()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnguardar_Clicked(object sender, EventArgs e)
+        {
+            Location location = null;
+            try
+            {
+                 location = await Geolocation.GetLastKnownLocationAsync();
+
+                if (location != null &&
+                    !String.IsNullOrEmpty(descripcion_corta.Text)&&
+                    !String.IsNullOrEmpty(descripcion_larga.Text))
+                {
+                    latitud.Text = Convert.ToString(location.Latitude);
+                    longitud.Text = Convert.ToString(location.Longitude);
+
+                }
+                else
+                { 
+                    if(location != null)
+                    {
+                        await DisplayAlert("Error", "Error no esta activo el gps", "OK");
+                    }
+                    else if(String.IsNullOrEmpty(descripcion_corta.Text))
+                    {
+                        await DisplayAlert("Error", "Error sin descripcion corta", "OK");
+                    }
+                    else if(String.IsNullOrEmpty(descripcion_larga.Text))
+                    {
+                        await DisplayAlert("Error", "Error sin descripcion larga", "OK");
+                    }
+                }
+            }
+            catch (Exception ex)
+
+            {
+                await DisplayAlert("Error", "Error no esta activo el gps", "OK");
+
+            }
+
+        }
+
+        private void btnver_Clicked(object sender, EventArgs e)
+        {
+
+        }
+     }
+}
+
